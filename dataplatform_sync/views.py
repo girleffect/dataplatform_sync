e54cbb10577b07a5a80992e62d85f5c6094d01f9
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-11-05 12:12:08 +0200

    update views rename form class

--- dataplatform_sync/views.py
+++ dataplatform_sync/views.py
@@ -1,18 +1,3 @@
-"""dataplatform_sync URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 import importlib
 from celery import current_app
 
@@ -33,7 +18,7 @@ task_choices = list(
 )
 
 
-class IndexForm(forms.Form):
+class TaskForm(forms.Form):
     delay = forms.BooleanField(required=False)
     task = forms.ChoiceField(choices=task_choices)
 
@@ -55,7 +40,7 @@ class IndexForm(forms.Form):
 
 
 class TaskView(FormView):
-    form_class = IndexForm
+    form_class = TaskForm
     template_name = 'index.html'
     success_url = reverse_lazy('tasks')
 
